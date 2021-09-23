0bad6233a181abcd68758b7b00e6217ca65f18a5
sms-mpinas <mpinascisneros@mpinasc-ltm1oma.internal.salesforce.com>
2021-09-22 22:38:46 -0300

    berseker version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,55 +2,54 @@ import './App.css';
 import React from 'react';
 import PlacesAutocomplete from 'react-places-autocomplete';
 import scriptLoader from 'react-async-script-loader';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App({isScriptLoaded, isScriptLoadSucceed }) {
-  const [address, setAddress] = React.useState("");
 
-  const [address1, setAddress1] = React.useState("");
+  const [origin, setOrigin] = React.useState("");
+
+  const [destination, setDestination] = React.useState("");
+
+  const [distance, setDistance] = React.useState("");
   
-  const handleChange = (value) => {
-    setAddress(value);
+  const originChange = (value) => {
+    setOrigin(value);
   }
 
-  const handleSelect = (value) => {
-    setAddress(value);
+  const originSelect = (value) => {
+    setOrigin(value);
   }
-  const handleChange1 = (value) => {
-    setAddress1(value);
-  }
-
-  const handleSelect1 = (value) => {
-    setAddress1(value);
+  const destinationChange = (value) => {
+    setDestination(value);
   }
 
-  function autocompletePlaces(address, handleChange, handleSelect) {
+  const destinationSelect = (value) => {
+    setDestination(value);
+  }
+   
+  async function accion () {
+    console.log(origin)
+    console.log(destination)
+    const res = await axios.get('http://localhost:4000/api/recorrido?origen='+origin+'&destino='+destination);
+    console.log(res.data.distance)
+    setDistance(res.data.distance)
+  }
+
+  function autocompletePlaces(location, handleChange, handleSelect) {
     return(
-      <PlacesAutocomplete 
-        value={address} 
-        onChange={handleChange} 
-        onSelect={handleSelect}
-        >
-        {({ 
-            getInputProps, 
-            suggestions, 
-            getSuggestionItemProps,
-            loading
-          }) => (
+      <PlacesAutocomplete value={location} onChange={handleChange} onSelect={handleSelect}>
+        {({ getInputProps, suggestions, getSuggestionItemProps,loading}) => (
               <div>
-                <input
-                {...getInputProps({
-                  placeholder: 'Enter Address ...'
-                })}
-                />
+                <input {...getInputProps({ placeholder: 'Enter Address ...'})}/>
                 <div>
                 {loading && <div>Loading...</div>}
                 {suggestions.map((suggestion) => {
                   const style = suggestion.active 
                     ? { backgroundColor: "#8ba4cc", cursor: "pointer"} 
                     : { backgroundColor: "#ffffff", cursor: "pointer"};
-                  console.log(suggestion.description)
                   return (
-                    <div {...getSuggestionItemProps(suggestion, {style})}>
+                    <div {...getSuggestionItemProps(suggestion, {style})} >
                       {suggestion.description}
                     </div>
                   );
@@ -65,8 +64,10 @@ function App({isScriptLoaded, isScriptLoadSucceed }) {
   if(isScriptLoaded && isScriptLoadSucceed){
     return (
     <div>
-      {autocompletePlaces(address, handleChange, handleSelect)}
-      {autocompletePlaces(address1, handleChange1, handleSelect1)}
+      {autocompletePlaces(origin, originChange, originSelect)}
+      {autocompletePlaces(destination, destinationChange, destinationSelect)}
+      <button onClick={() => accion()}>Enviar</button>
+      <div>{distance}</div>
     </div>
     );
   }else{
